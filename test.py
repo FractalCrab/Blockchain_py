60474a5bf4d046cd6d48ad999c4c6a8067338a32
Abhinandan Sharma <40931723+FractalCrab@users.noreply.github.com>
2019-11-02 17:59:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,5 +8,5 @@ new_transactions = [{'patient':"test",
 my_blockchain = Blockchain()
 my_blockchain.add_block(new_transactions)
 my_blockchain.print_blocks()
-my_blockchain.chain[1].transactions = 'fake_transactions'
+my_blockchain.chain[1].transactions = 'fake'
 my_blockchain.check_chain()
