869ea59e3b272263cda74e132ee022e38ade2840
Abhinandan <abhinandan@Abhinandans-MacBook-Pro.local>
2019-11-02 16:43:41 +0530

    hashing

--- block.py
+++ block.py
@@ -1,6 +1,7 @@
 '''Importing datetime library for recording timestamp'''
 from datetime import datetime
 
+from hashlib impirt sha256
 
 #creating a block for blockhchain
 
@@ -10,3 +11,8 @@ class Block:
         self.previous_hash=previous_hash
         self.nonce=nonce
         self.timestamp=datetime.now()
+    #generating hash for the block
+    def generate_hash(self):
+        block_details=str(self.timestamp)+str(self.transcations)+str(self.previous_hash)+str(self.nonce)
+        block_hash=sha256(block_details.encode())
+        return block_hash.hexdigest()
